d7852a7bdd7da85ffa34fe56d97b59385ddbf387
Tyler McCarthy <mccartty@mail.gvsu.edu>
2015-10-24 10:17:29 -0400

    BLARBL

--- ai.py
+++ ai.py
@@ -1,17 +1,15 @@
+#!/usr/bin/python
 import argparse
 import json
 import sys
 
+dim = 8
+
+# json board
 boardstr = sys.argv[3]
 boardstr = json.dumps(board)
 
-color = sys.argv[5]
-if color == 'black':
-    color = 'b'
-else color = 'w'
-
-dim = 8
-
+# python board
 squares = boardstr['squares']
 board = []
 boardstart = 0
@@ -19,17 +17,44 @@ for i in range(dim):
     board[i] = squares[0:dim]
     boardstart = boardstart + dim
 
+# our color
+color = sys.argv[5]
+if color == 'black':
+    color = 'b'
+else color = 'w'
 
-def is_valid(x, y):
-    return
+"""
 
-def solve():
+
+# Valid list of moves
+def create_valids():
+    empties = [] 
+    for x in range(8):
+        for y in range(8):
+            if board[x][y] == '-':
+                empties.add (x, y)
+    
+    for i in empties:
+        
+    
+
+
+    # build horizontal and vertical lists
+        horizontal = 
+
+    # if it's touching other piece
     for i in board:
         for square in i:
-            if  
+            if 
 
+    # if inline with other piece
+
+"""
+
+def solve():
     # return the move
-    exit(move)
+    
+    exit(20)
 
 
 if __name == '__main__':
